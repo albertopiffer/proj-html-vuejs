121431249b2560760be4b7785bb0fa36ada0f7de
albertopiffer <albertopiffer96@gmail.com>
2023-03-15 02:47:58 +0100

    FEAT header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script>
+
+import moversHeader from './components/moversHeader.vue'
+import moversMain from './components/moversMain.vue'
+import moversFooter from './components/moversFooter.vue'
+
+export default {
+  components: {
+    moversHeader,
+    moversMain,
+    moversFooter,
+  }
+}
+
 </script>
 
 <template>
   <div>
+    <moversHeader />
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@use './style/main.scss';
+</style>
---
+++ src/components/moversHeader.vue
@@ -0,0 +1,48 @@
+<template>
+    <header>
+        <div class="container">
+            <p>Call us for a Free Quote: 1.800.555.6789</p>
+            <ul>
+                <li><font-awesome-icon icon="fa-brands fa-facebook-f" /></li>
+                <li><font-awesome-icon icon="fa-brands fa-twitter" /></li>
+                <li><font-awesome-icon icon="fa-brands fa-instagram" /></li>
+                <li><font-awesome-icon icon="fa-brands fa-youtube" /></li>
+            </ul>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+@use '../style/partials/colors' as *;
+
+header {
+    height: 47px;
+    background-color: $lima;
+
+    font-size: 13px;
+    color: white;
+
+    padding: 0 30px;
+
+    .container {
+        height: 100%;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        ul {
+            display: flex;
+            justify-content: flex-end;
+
+            gap: 20px;
+
+            font-size: 16px;
+        }
+    }
+}
+</style>
